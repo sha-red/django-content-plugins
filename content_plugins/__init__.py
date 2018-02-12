e24072492dff108e03592954a7c7a473bd2461d6
Erik Stein <erik@classlibrary.net>
2018-02-12 17:42:08 +0100

    Version 0.2.0.

--- content_plugins/__init__.py
+++ content_plugins/__init__.py
@@ -1,6 +1,10 @@
 from django.conf import settings
 
 
+VERSION = (0, 2, 0)
+__version__ = '.'.join(map(str, VERSION))
+
+
 USE_TRANSLATABLE_FIELDS = getattr(settings, 'CONTENT_USE_TRANSLATABLE_FIELDS', True)
 
 # TODO Implement translatable AutoSlugField: USE_TRANSLATABLE_SLUG_FIELDS = getattr(settings, 'CONTENT_USE_TRANSLATABLE_SLUG_FIELDS', True)
